f1877e24c38fd74bd7ae81ab0a4d798447d95f8e
Your Name <you@example.com>
2024-06-03 17:30:39 +0100

    first commit

---
+++ responsive-portfolio-website-patrick-main/assets/js/script.js
@@ -0,0 +1,19 @@
+// let name = prompt("INPUT YOUR NAME")
+
+var typed = new Typed(".auto-type", {
+  strings: ["Ayomide Adeniran"],
+  typeSpeed: 300,
+  backSpeed: 20,
+  loop: true
+});
+var typed = new Typed(".userauto-type", {
+  strings: [`${name}`],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true
+});
+
+
+
+
+
